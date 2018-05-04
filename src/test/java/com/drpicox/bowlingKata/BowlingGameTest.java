00479acfeec3e629a42050d7515187d894e3e7b0
xaviherasguerrero <xaviherasguerrero@gmail.com>
2018-05-04 09:05:31 +0200

    slide26

--- src/test/java/com/drpicox/bowlingKata/BowlingGameTest.java
+++ src/test/java/com/drpicox/bowlingKata/BowlingGameTest.java
@@ -18,9 +18,7 @@ public class BowlingGameTest extends TestCase {
     }
 
     public void testGutterGame() throws Exception {
-        int n = 20;
-        int pins = 0;
-        rollMany(n, pins);
+        rollMany(20, 0);
         assertEquals(0, g.score());
     }
 
@@ -34,5 +32,6 @@ public class BowlingGameTest extends TestCase {
             g.roll(1);
         assertEquals(20, g.score());
     }
+
 
 }
